8af3c1e359f6252af6f22d03feeabc4bf2af77d8
Nameera Pathan <66337671+dedsecwrench@users.noreply.github.com>
2022-05-21 00:29:59 +0530

    portfolio

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -20,17 +20,17 @@ export const Header = () => {
     }
     return (
     <div>
-         <div>
+        <div>
             <img className='home-img' src={mac} alt="" style={{borderBottom:"1px solid #706e6e"}} />
             <header className="header">
                 <nav className="navbar navbar-expand-lg fixed-top py-3">
                     <div className="container">
                         <div id="navbarSupportedContent navbar-nav ml-auto" className="collapse navbar-collapse">
                             <span className='mid'>
-                                <button class="nav-item font-weight-bold btn from-center" onClick={()=>goToHome()}>Home</button>
-                                <button class="nav-item font-weight-bold btn from-center" onClick={()=>goToAbout()}>About</button>
-                                <button class="nav-item font-weight-bold btn from-center" onClick={()=>goToProjects()}>Portfolio</button>
-                                <button class="nav-item font-weight-bold btn from-center" onClick={()=>goToContact()}>Contact</button></span>
+                                <button className="nav-item font-weight-bold btn from-center" onClick={()=>goToHome()}>Home</button>
+                                <button className="nav-item font-weight-bold btn from-center" onClick={()=>goToAbout()}>About</button>
+                                <button className="nav-item font-weight-bold btn from-center" onClick={()=>goToProjects()}>Portfolio</button>
+                                <button className="nav-item font-weight-bold btn from-center" onClick={()=>goToContact()}>Contact</button></span>
                         </div>
                     </div>
                 </nav>
@@ -45,7 +45,6 @@ export const Header = () => {
            <About/><br/><br/><br/>
            <Projects/><br/><br/>
            <Footer/>
-
 
     </div>
 
