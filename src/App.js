0bb97b6f7a018d6dddc7d477de787907b9122c45
Nameera Pathan <66337671+dedsecwrench@users.noreply.github.com>
2022-05-31 16:30:03 +0530

    added loading symbol

--- src/App.js
+++ src/App.js
@@ -4,18 +4,40 @@ import About from './components/About';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
+import {useEffect,useState} from 'react'
 
 function App() {
-  return (
-    <div>
-        <NavbarComp/>
-        <Home/>
-        <About/>
-        <Projects/>
-        <Footer/>
-       
+
+  const [isLoading,setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+  }, [isLoading]);
+
+  if(isLoading){
+    return(
+      <div class="loader mt-80">
+      <div class="outer"></div>
+      <div class="middle"></div>
+      <div class="inner"></div>
     </div>
-  );
+    )
+  }
+  else{
+    return (
+      <div>
+          <NavbarComp/>
+          <Home/>
+          <About/>
+          <Projects/>
+          <Footer/>
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
