73039b31d1641fde3028c6adaffb87cfadf43d62
Nameera Pathan <66337671+dedsecwrench@users.noreply.github.com>
2022-06-18 21:39:42 +0530

    added weather app

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import clock from './images/clock.png'
 import AddStd from './images/AddStudentApp.png'
 import TodoApp from './images/todoapp.png'
 import Blog from './images/blog.png'
+import weather from './images/weather.png'
 // import Card from '@mui/material/Card';
 // import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -50,30 +51,28 @@ const Projects = () => {
       </div>
 
 
-     
       <div className="box">
-      <a href="https://react-j-s-blog.netlify.app" target="_blank" style={{textDecoration: "none"}}>
+      <a href="https://weather-app-ow.netlify.app" target="_blank" style={{textDecoration: "none"}}>
         <span className='span1'></span>
         <div className="content">
-          <h1>Blog App</h1>
+          <h2>Weather App</h2>
           <CardMedia
-              className='pb-2'
+          className='pb-2'
               component="img"
               height="190"
-              image={Blog}
+              image={weather}
               alt=""
-              style={{borderRadius:"4px 4px 5px 5px"}}
+              style={{borderRadius:"4px"}}
             />
           <div className='flex gap-2 flex-wrap'>
           <img className='html' src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png" alt=""/>
           <img className='html' src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png" alt=""/>
           <img className='js' src={js} alt=""/>
           <img className='js' src="https://i0.wp.com/www.primefaces.org/wp-content/uploads/2017/09/feature-react.png?ssl=1" alt=""/>
-          </div>  
+          </div> 
         </div>
         </a>
       </div>
-
 
 
       <div className="box">
@@ -105,8 +104,9 @@ const Projects = () => {
 
   
       <div className='container6 ml-20 pb-5'>
-      <a href="https://redux-add-del-edit.netlify.app" target="_blank" style={{textDecoration: "none"}}>
+     
       <div className="box">
+      <a href="https://redux-add-del-edit.netlify.app" target="_blank" style={{textDecoration: "none"}}>
         <span className='span1'></span>
         <div className="content">
           <h2>Student App</h2>
@@ -127,8 +127,32 @@ const Projects = () => {
           </div> 
           {/* <a href="#">View Details</a> */}
         </div>
+        </a>
       </div>
-      </a>
+
+      <div className="box">
+      <a href="https://react-j-s-blog.netlify.app" target="_blank" style={{textDecoration: "none"}}>
+        <span className='span1'></span>
+        <div className="content">
+          <h1>Blog App</h1>
+          <CardMedia
+              className='pb-2'
+              component="img"
+              height="190"
+              image={Blog}
+              alt=""
+              style={{borderRadius:"4px 4px 5px 5px"}}
+            />
+          <div className='flex gap-2 flex-wrap'>
+          <img className='html' src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png" alt=""/>
+          <img className='html' src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png" alt=""/>
+          <img className='js' src={js} alt=""/>
+          <img className='js' src="https://i0.wp.com/www.primefaces.org/wp-content/uploads/2017/09/feature-react.png?ssl=1" alt=""/>
+          </div>  
+        </div>
+        </a>
+      </div>
+
       </div>
 
 
